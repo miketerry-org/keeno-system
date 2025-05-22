0c8292dfb5c2545ce7b520569bad7748898fefea
Mike Terry <support@miketerry.org>
2025-05-22 07:20:39 -0500

    added findFiles function

--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 
 "use strict";
 
-// export the node environment as a lowercase constant
-const envMode = process.env.NODE_ENV?.toLowerCase();
+// load all necessary modules
+const findFiles = require("./lib/findFiles");
+const {
+  envMode,
+  isDevelopment,
+  isProduction,
+  isTesting,
+} = require("./lib/envMode");
 
-// export constants for isDevelopment, isProduction and isTesting
-const isDevelopment = envMode === "dev" || envMode === "development";
-const isProduction = envMode === "prod" || envMode === "production";
-const isTesting = envMode === "test" || envMode === "testing";
-
+// convenient global variable for application start directory
 const __workdir = process.cwd();
 
+// global logging object
+const log = console;
+
+// replace the global logger
+function setLog(value) {
+  log = value;
+  return log;
+}
+
+// display error message and exit process with an error code
 function fatal(message) {
   console.error(`Fatal Error: ${message}`);
   process.exit(1);
 }
 
+// display error message and exit process with specific error code
 function halt(code) {
   console.error(`ERROR ${code}: Terminating program execution .`);
   process.exit(code);
@@ -31,4 +44,7 @@ module.exports = {
   isTesting,
   fatal,
   halt,
+  findFiles,
+  log,
+  setLog,
 };
